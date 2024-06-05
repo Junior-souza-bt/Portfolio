dc7e5e8cd6109e50f4524acd5b2f6d5cfc3115cd
Junior Souza <86856904+JuniorSouza21@users.noreply.github.com>
2024-06-04 23:05:56 -0300

    Incrementacao
    botão de voltar ao topo da pagina.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Alternar título
 function mudarTitulo() {
     const titles = ["Portfólio Pessoal", " Seja Bem-vindo(a)! "]; 
     let index = 0;
@@ -9,3 +10,21 @@ function mudarTitulo() {
 }
 
 mudarTitulo();
+
+//botão voltar ao topo
+jQuery(document).ready(function(){
+
+    jQuery(window).scroll(function(){
+      if (jQuery(this).scrollTop() > 100) {
+        jQuery('a[href="#top"]').fadeIn();
+      } else {
+        jQuery('a[href="#top"]').fadeOut();
+      }
+    });
+  
+    jQuery('a[href="#top"]').click(function(){
+      jQuery('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+  
+  });	
